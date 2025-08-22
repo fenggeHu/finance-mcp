0c191761fee783dc342ae734513088caf0750012
max.hu <pbfoxhu@gmail.com>
2025-08-22 16:48:07 +0800

    for tests

--- fmcp/main_server.py
+++ fmcp/main_server.py
@@ -4,14 +4,14 @@ from fmcp import yahoo_finance_server
 from fmcp import cn_finance_server
 
 # 创建主 Server
-server = FastMCP("finance mcp server")
+mcp = FastMCP("finance mcp server")
 
 # 有些Client不支持引入的多级mcp server
 # 挂载 us/
-server.import_server(yahoo_finance_server.yfinance_mcp, prefix="us/")
+mcp.import_server(yahoo_finance_server.yfinance_mcp, prefix="us/")
 # 挂载 cn/
-server.import_server(cn_finance_server.cn_finance_mcp, prefix="cn/")
+mcp.import_server(cn_finance_server.cn_finance_mcp, prefix="cn/")
 
 if __name__ == "__main__":
     # 只需运行主 Server
-    server.run(transport="sse", host="127.0.0.1", port=8000)
+    mcp.run(transport="sse", host="127.0.0.1", port=8000)

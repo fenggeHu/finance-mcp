76617dedf34e4563fd7ae7311100ed88226cbddc
max.hu <pbfoxhu@gmail.com>
2025-08-22 16:33:47 +0800

    for tests

--- fmcp/cn_finance_server.py
+++ fmcp/cn_finance_server.py
@@ -2,3 +2,7 @@ from fastmcp import FastMCP
 
 # Create a china finance market server instance
 cn_finance_mcp = FastMCP(name="china finance mcp server")
+
+if __name__ == "__main__":
+    # 只需运行主 Server
+    cn_finance_mcp.run(transport="sse", host="127.0.0.1", port=8000)
